e2dc25e03fa75b504ab27216f6d5fa5bc7c25c27
Elif <157531406+ElifDedeoglu@users.noreply.github.com>
2025-07-18 21:30:05 +0300

    Delete routes1 directory

--- routes1/mealRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { authenticateToken } = require("../middleware/authMiddleware");
-const { addMeal, getDailyReport } = require("../controllers1/mealController");
-
-router.post("/", authenticateToken, addMeal);          // Öðün ekle
-router.get("/report", authenticateToken, getDailyReport); // Rapor getir
-
-module.exports = router;
